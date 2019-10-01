57f10b6d08155fcfaf98f907e876b1d74e4bba79
Gavrilov.NI <pelmeshka1110@gmail.com>
2019-10-01 23:37:37 +0300

    No changes #3 new branch

--- src/tset.cpp
+++ src/tset.cpp
@@ -4,7 +4,7 @@
 //   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
 //
 // Множество - реализация через битовые поля
-//Changes for GitHub Pull Request#2
+//Changes for GitHub Pull Request#3
 #include "tset.h"
 
 TSet::TSet(int mp) : BitField(mp)
