58d32b91aee16935bffca0a5d1d4836a402da639
Mariya Kuznetsova <56151612+agrht@users.noreply.github.com>
2019-12-06 09:53:18 +0300

    Update tset.h

--- include/tset.h
+++ include/tset.h
@@ -1,43 +1,47 @@
 // ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
 //
-// tset.h - Copyright (c) Гергель В.П. 07.05.2001
+// tbitfield.h - Copyright (c) Гергель В.П. 07.05.2001
 //   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
 //
-// Множество
-
-#ifndef __SET_H__
-#define __SET_H__
-
-#include "tbitfield.h"
-
-class TSet
+// Битовое поле
+#ifndef __BITFIELD_H__
+#define __BITFIELD_H__
+#include <iostream>
+using namespace std;
+typedef unsigned int TELEM;
+class TBitField
 {
 private:
-  int MaxPower;       // максимальная мощность множества
-  TBitField BitField; // битовое поле для хранения характеристического вектора
+  int  BitLen; // длина битового поля - макс. к-во битов
+  TELEM *pMem; // память для представления битового поля
+  int  MemLen; // к-во эл-тов Мем для представления бит.поля
+  // методы реализации
+  int   GetMemIndex(const int n) const; // индекс в pМем для бита n       (#О2)
+  TELEM GetMemMask (const int n) const; // битовая маска для бита n       (#О3)
 public:
-  TSet(int mp);
-  TSet(const TSet &s);       // конструктор копирования
-  TSet(const TBitField &bf); // конструктор преобразования типа
-  operator TBitField();      // преобразование типа к битовому полю
+	TBitField(int len);
+		//                                   (#О1)
+	TBitField(const TBitField& a);
+ //                                   (#П1)
+  ~TBitField();                      //                                    (#С)
   // доступ к битам
-  int GetMaxPower(void) const;     // максимальная мощность множества
-  void InsElem(const int Elem);       // включить элемент в множество
-  void DelElem(const int Elem);       // удалить элемент из множества
-  int IsMember(const int Elem) const; // проверить наличие элемента в множестве
-  // теоретико-множественные операции
-  int operator== (const TSet &s) const; // сравнение
-  int operator!= (const TSet &s) const; // сравнение
-  TSet& operator=(const TSet &s);  // присваивание
-  TSet operator+ (const int Elem); // объединение с элементом
-                                   // элемент должен быть из того же универса
-  TSet operator- (const int Elem); // разность с элементом
-                                   // элемент должен быть из того же универса
-  TSet operator+ (const TSet &s);  // объединение
-  TSet operator* (const TSet &s);  // пересечение
-  TSet operator~ (void);           // дополнение
-
-  friend istream &operator>>(istream &istr, TSet &bf);
-  friend ostream &operator<<(ostream &ostr, const TSet &bf);
+  int GetLength(void) const;      // получить длину (к-во битов)           (#О)
+  void SetBit(const int n);       // установить бит                       (#О4)
+  void ClrBit(const int n);       // очистить бит                         (#П2)
+  int  GetBit(const int n) const; // получить значение бита               (#Л1)
+  // битовые операции
+  int operator==(const TBitField &bf) const; // сравнение                 (#О5)
+  int operator!=(const TBitField &bf) const; // сравнение
+  TBitField& operator=(const TBitField &bf); // присваивание              (#П3)
+  TBitField  operator|(const TBitField &bf); // операция "или"            (#О6)
+  TBitField  operator&(const TBitField &bf); // операция "и"              (#Л2)
+  TBitField  operator~(void) const;                // отрицание                  (#С)
+  friend istream &operator>>(istream &istr, TBitField &bf);       //      (#О7)
+  friend ostream &operator<<(ostream &ostr, const TBitField &bf); //      (#П4)
 };
+// Структура хранения битового поля
+//   бит.поле - набор битов с номерами от 0 до BitLen
+//   массив pМем рассматривается как последовательность MemLen элементов
+//   биты в эл-тах pМем нумеруются справа налево (от младших к старшим)
+// О8 Л2 П4 С2
 #endif
