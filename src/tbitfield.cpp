ad02f937ec3bd65ee19eac9005879a8546b826a9
BeketovE <57558717+BeketovE@users.noreply.github.com>
2020-10-06 16:54:55 +0300

    laba

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -40,11 +40,12 @@ TBitField::~TBitField()
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
- if (n < 0 || n >= Bitlen)
+ if (n >= 0 && n < BitLen)
  {
-  throw "Error";
+  return n / (8 * sizeof(TELEM));
  }
- return (floor((double)n / (sizeof(TELEM) * 8)));
+ else
+  throw - 1;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
