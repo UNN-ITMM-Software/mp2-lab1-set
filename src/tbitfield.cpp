8f6d8609e51a04921edd117f555e71f1e90dd641
AntonMurzinov <murzinov.toni@inbox.ru>
2020-10-06 16:27:30 +0300

    f12

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -6,14 +6,20 @@
 
 TBitField::TBitField(int len)
 {
-  if (len < 0) throw - 1;
-  BitLen = len;
-  MemLen = (len + (sizeof(TELEM) * 8 - 1)) / (sizeof(TELEM) * 8);
-  pMem = new TELEM[MemLen];
-  for (int i = 0; i < MemLen; i++)
+  if (len > 0)
   {
-	pMem[i] = 0;
-  }
+	BitLen = len;
+	MemLen = ceil((double)BitLen / 8 * sizeof(TELEM));
+	pMem = nullptr;
+	pMem = new TELEM[MemLen];
+	if (pMem != nullptr)
+	  for (int i = 0; i < MemLen; i++)
+		pMem[i] = 0;
+	else
+	  throw - 1;
+	}
+  else
+	throw - 1;
 }
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
