6d2a2cd4be543c213a171bd0586c42316c3506f9
Мария Страшенко <strash@mail.ru>
2020-10-07 15:42:52 +0300

    Исправленный код

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -49,7 +49,7 @@ TBitField::~TBitField()
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
   if (n >= 0 && n < BitLen)
-    return n / (8 * sizeof(TELEM));
+    return (floor)(n / (8.0 * sizeof(TELEM)));
   else
     throw - 1;
 }
