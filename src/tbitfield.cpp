fd4a1490c640cbb817e8c5c236c1e11d43e629eb
Егор Косарев <kosegor2003@yandex.ru>
2022-09-21 15:56:13 +0300

    Написаны битовые поля

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -13,31 +13,57 @@ static TBitField FAKE_BITFIELD(1);
 
 TBitField::TBitField(int len)
 {
+    if (len > 0)
+    {
+        MemLen = len;
+        BitLen = len * sizeof(TELEM) * 8;
+        pMem = new TELEM[MemLen];
+        for (int i = 0; i < MemLen; i++)
+        {
+            pMem[i] = 0;
+        }
+    }
+    else
+        throw "Error";
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+    MemLen = bf.MemLen;
+    BitLen = bf.BitLen;
+    pMem = new TELEM(MemLen);
+    for (int i = 0; i < MemLen; i++)
+    {
+        pMem[i] = bf.pMem[i];
+    }
 }
 
 TBitField::~TBitField()
 {
+    if (pMem != 0) delete[] pMem;
+    MemLen = 0;
+    BitLen = 0;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
-    return FAKE_INT;
+    if (n < 0 || n >= BitLen)
+        throw "Error";
+    return n >> 5;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
-    return FAKE_INT;
+    if (n < 0 || n >= BitLen)
+        throw "Error";
+    return 1 << (n & 31);
 }
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return FAKE_INT;
+    return MemLen;
 }
 
 void TBitField::SetBit(const int n) // установить бит
