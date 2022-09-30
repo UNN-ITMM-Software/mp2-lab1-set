98b08a0901c7f7b5fbdbe4ad3bef5aab4e018ab9
mahbhlddnhakkh <>
2022-09-30 20:30:03 +0300

    Try 1

--- include/tbitfield.h
+++ include/tbitfield.h
@@ -9,6 +9,7 @@
 #define __BITFIELD_H__
 
 #include <iostream>
+#include "AdditionalFunctions.h"
 
 using namespace std;
 
@@ -18,8 +19,9 @@ class TBitField
 {
 private:
   int  BitLen; // длина битового поля - макс. к-во битов
-  TELEM *pMem; // память для представления битового поля
+  TELEM *pMem = nullptr; // память для представления битового поля
   int  MemLen; // к-во эл-тов Мем для представления бит.поля
+  static size_t GET_INDEX_CONST;
 
   // методы реализации
   int   GetMemIndex(const int n) const; // индекс в pМем для бита n       (#О2)
