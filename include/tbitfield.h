ff4b05d6338fc806fd413acf97f8336951dd32db
Aleksandra <ERTY-00@yandex.ru>
2023-12-25 19:16:49 +0300

    lab1

--- include/tbitfield.h
+++ include/tbitfield.h
@@ -12,14 +12,14 @@
 
 using namespace std;
 
-typedef unsigned int TELEM;
+typedef unsigned  int TELEM;
 
 class TBitField
 {
 private:
   int  BitLen; // длина битового поля - макс. к-во битов
-  TELEM *pMem; // память для представления битового поля
-  int  MemLen; // к-во эл-тов Мем для представления бит.поля
+  TELEM *pMem; // память для представления битового поля(указатель на память)
+  int  MemLen; // к-во эл-тов Мем для представления бит.поля(Mem-память)
 
   // методы реализации
   int   GetMemIndex(const int n) const; // индекс в pМем для бита n       (#О2)
